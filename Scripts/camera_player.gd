b1781db7aac75864ca3c27aa6b13f07619e7be77
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2026-01-16 17:10:13 -0300

    Update camera position

--- Scripts/camera_player.gd
+++ Scripts/camera_player.gd
@@ -1,11 +1,21 @@
 extends Camera3D
 
+@export var camera_anchor : Node3D
 
-# Called when the node enters the scene tree for the first time.
+@export var distance_treshold : int = 12
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if not camera_anchor:
+		push_error("Camera anchor not found!")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	global_position = global_position.move_toward(camera_anchor.global_position, calculate_speed() * delta)
+
+func calculate_speed() -> float:
+	# Quero uma velocidade entre [2, 10], de acordo com a distância
+	# A função que define isso é: y = ln(x + 1) * 3
+	# Estou usando a distância ao quadrado para poupar cálculos
+	var distance_to_player = global_position.distance_squared_to(camera_anchor.global_position)
+	if distance_to_player > distance_treshold:
+		return 10.0
+	return log(distance_to_player + 1) * 3
