27a255f3e5de1e6e9bc0169f7eced436d9270202
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-01-30 16:50:16 -0300

    add game win by points collected trigger (set to 4 for dev)

--- Scripts/game_controller.gd
+++ Scripts/game_controller.gd
@@ -1,1 +1,14 @@
 extends Node3D
+
+var POINTS_TO_WIN = 4
+
+func _ready() -> void:
+	%ScoreController.connect('score_update', check_game_end)
+	
+func check_game_end(points : int) -> void:
+	print('checa se ganhou')
+	if points >= POINTS_TO_WIN:
+		end_game()
+		
+func end_game():
+	print('Jogadores Ganharam!')
